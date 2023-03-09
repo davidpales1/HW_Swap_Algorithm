8304b680b885a1ce30332591711b73071740d6a5
jihad <david.pales1@gmail.com>
2023-03-09 21:27:49 +0100

    fgdf

--- Code/HW_Main.cpp
+++ Code/HW_Main.cpp
@@ -66,56 +66,51 @@ bool isNumber(string s)
     float x;
     return (ss >> x) && ss.eof();
 }
-void createArray(int choice, int arraySize) {
-    
+// 
+void getAndprintIntList(int arraySize) {
     int* arrInt = new int[arraySize];
+    // create an integer array
+    for (int i = 0; i < arraySize; i++) {
+        cout << "Enter an integer value for element " << i << ": ";
+        cin >> arrInt[i];
+    }
+
+    sort_integers(arrInt, arraySize);
+    // print the integer array
+    cout << "The integer array you created is: ";
+    for (int i = 0; i < arraySize; i++) {
+        cout << arrInt[i] << " ";
+    }
+    delete[] arrInt;
+}
+void getAndprintStringList(int arraySize) {
     string* arrStr = new string[arraySize];
+    // create a string array
+    for (int i = 0; i < arraySize; i++) {
+        input = "";
+        cout << "Enter a string value for element " << i << ": ";
+        cin >> input;
+        arrStr[i] = trim(input);
 
+    }
+    sort_strings(arrStr, arraySize);
+    // print the string array
+    cout << "The string array you created is: ";
+    for (int i = 0; i < arraySize; i++) {
+        cout << arrStr[i] << " ";
+    }
+    delete[] arrStr;
+}
+void createArray(int choice, int arraySize) {
     if (choice == 1) {
-        
-        // create an integer array
-        if (arraySize >= 0 && arraySize < 100) {
-            for (int i = 0; i < arraySize; i++) {
-                cout << "Enter an integer value for element " << i << ": ";
-                cin >> arrInt[i];
-            }
-        }
-        else {
-            cout << "The maximam size of the array should not exceed 100";
-        }
-
-        
-        sort_integers(arrInt, arraySize);
-        // print the integer array
-        cout << "The integer array you created is: ";
-        for (int i = 0; i < arraySize; i++) {
-            cout << arrInt[i] << " ";
-        }
+        getAndprintIntList(arraySize);
     }
     else if (choice == 2) {
-      
-        // create a string array
-        if (arraySize >= 0 && arraySize < 100) {
-            for (int i = 0; i < arraySize; i++) {
-                cout << "Enter a string value for element " << i << ": ";
-                cin >> arrStr[i];
-            }
-        }
-        else {
-            cout << "The maximam size of the array should not exceed 100";
-        }
-        sort_strings(arrStr, arraySize);
-        // print the string array
-        cout << "The string array you created is: ";
-        for (int i = 0; i < arraySize; i++) {
-            cout << arrStr[i] << " ";
-        }
+        getAndprintStringList(arraySize);
     }
     else {
         cout << "Invalid choice. Please enter 1 for integer or 2 for string." << endl;
     }
-    delete[] arrInt;
-    delete[] arrStr;
 }
 // Function/Method Declaration - END
 
